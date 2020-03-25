7e29a112f19757d91d0f0fc0a38c404dd734a9a4
Przemyslaw Bądaruk <przemekkth@gmail.com>
2020-03-25 18:46:41 +0100

    Add Splash Screen.png/cs/scene

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    [SerializeField] float timeToWait = 4f;
+    int currentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if( currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    private void LoadNextScene()
+    {
+        int nextSceneIndex = currentSceneIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
